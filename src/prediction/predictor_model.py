1793be850cb53a1ed013820f718699fb4bf5f1f2
Mo <mohamedabdelhamid3030@gmail.com>
2024-08-19 15:10:33 +0300

    created imbalanced branch

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 from piml.models import ReluDNNClassifier
 from sklearn.exceptions import NotFittedError
+from sklearn.metrics import f1_score
+
 
 warnings.filterwarnings("ignore")
 
@@ -37,6 +39,8 @@ class Classifier:
         threshold: float = 0.1,
         verbose: bool = False,
         random_state: int = 0,
+        decision_threshold: Optional[float] = 0.5,
+        positive_class_weight: Optional[float] = 1.0,
         **kwargs,
     ):
         """Construct a new ReluDNNClassifier.
@@ -61,6 +65,10 @@ class Classifier:
             threshold (int): Threshold value for performing IHT, required when IHT is True.
             verbose (bool): Whether to display training statistics (loss) or not.
             random_state (int): Determines random number generation for weights and bias initialization.
+            decision_threshold (float, optional): The decision threshold for
+                the positive class. Defaults to 0.5.
+            positive_class_weight (float, optional): The weight of the positive
+                class. Defaults to 1.0.
         """
         self.feature_names = feature_names
         self.feature_types = feature_types
@@ -78,6 +86,8 @@ class Classifier:
         self.threshold = threshold
         self.verbose = verbose
         self.random_state = random_state
+        self.decision_threshold = decision_threshold
+        self.positive_class_weight = positive_class_weight
         self.kwargs = kwargs
         self.model = self.build_model()
         self._is_trained = False
@@ -112,18 +122,32 @@ class Classifier:
             train_inputs (pandas.DataFrame): The features of the training data.
             train_targets (pandas.Series): The labels of the training data.
         """
-        self.model.fit(train_inputs, train_targets)
+        sample_weight = train_targets.map({0: 1, 1: self.positive_class_weight})
+        self.model.fit(train_inputs, train_targets, sample_weight=sample_weight)
         self._is_trained = True
 
-    def predict(self, inputs: pd.DataFrame) -> np.ndarray:
+    def predict(
+        self,
+        inputs: pd.DataFrame,
+        decision_threshold: float = -1,
+    ) -> np.ndarray:
         """Predict class labels for the given data.
 
         Args:
             inputs (pandas.DataFrame): The input data.
+            decision_threshold (Optional float): Decision threshold for the
+                positive class.
+                Value -1 indicates use the default set when model was
+                instantiated.
         Returns:
             numpy.ndarray: The predicted class labels.
         """
-        return self.model.predict(inputs)
+        if decision_threshold == -1:
+            decision_threshold = self.decision_threshold
+        if self.model is not None:
+            prob = self.predict_proba(inputs)
+            labels = prob[:, 1] >= decision_threshold
+        return labels
 
     def predict_proba(self, inputs: pd.DataFrame) -> np.ndarray:
         """Predict class probabilities for the given data.
@@ -135,17 +159,32 @@ class Classifier:
         """
         return self.model.predict_proba(inputs)
 
-    def evaluate(self, test_inputs: pd.DataFrame, test_targets: pd.Series) -> float:
-        """Evaluate the binary classifier and return the accuracy.
+    def evaluate(
+        self,
+        test_inputs: pd.DataFrame,
+        test_targets: pd.Series,
+        decision_threshold: float = -1,
+    ) -> float:
+        """Evaluate the classifier and return the accuracy.
 
         Args:
             test_inputs (pandas.DataFrame): The features of the test data.
             test_targets (pandas.Series): The labels of the test data.
+            decision_threshold (Optional float): Decision threshold for the
+                positive class.
+                Value -1 indicates use the default set when model was
+                instantiated.
         Returns:
-            float: The accuracy of the binary classifier.
+            float: The accuracy of the classifier.
         """
+        if decision_threshold == -1:
+            decision_threshold = self.decision_threshold
         if self.model is not None:
-            return self.model.score(test_inputs, test_targets)
+            prob = self.predict_proba(test_inputs)
+            labels = prob[:, 1] >= decision_threshold
+            score = f1_score(test_targets, labels)
+            return score
+
         raise NotFittedError("Model is not fitted yet.")
 
     def save(self, model_dir_path: str) -> None:
@@ -245,7 +284,10 @@ def load_predictor_model(predictor_dir_path: str) -> Classifier:
 
 
 def evaluate_predictor_model(
-    model: Classifier, x_test: pd.DataFrame, y_test: pd.Series
+    model: Classifier,
+    x_test: pd.DataFrame,
+    y_test: pd.Series,
+    decision_threshold: float = -1,
 ) -> float:
     """
     Evaluate the classifier model and return the accuracy.
@@ -254,8 +296,23 @@ def evaluate_predictor_model(
         model (Classifier): The classifier model.
         x_test (pd.DataFrame): The features of the test data.
         y_test (pd.Series): The labels of the test data.
+        decision_threshold (Union(optional, float)): Decision threshold
+                for predicted label.
+                Value -1 indicates use the default set when model was
+                instantiated.
 
     Returns:
         float: The accuracy of the classifier model.
     """
-    return model.evaluate(x_test, y_test)
+    return model.evaluate(x_test, y_test, decision_threshold)
+
+
+def set_decision_threshold(model: Classifier, decision_threshold: float) -> None:
+    """
+    Set the decision threshold for the classifier model.
+
+    Args:
+        model (Classifier): The classifier model.
+        decision_threshold (float): The decision threshold.
+    """
+    model.decision_threshold = decision_threshold
